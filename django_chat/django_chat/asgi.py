c117d52207be0aaed6d96af76ba6baf4a0893684
JoynerRaj <joynerjohnraj04@gmail.com>
2026-03-27 12:32:32 +0530

    Fix WebSocket protocol for production

--- django_chat/django_chat/asgi.py
+++ django_chat/django_chat/asgi.py
@@ -1,14 +1,10 @@
 import os
-import django
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
-
-django.setup()
-
-import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
