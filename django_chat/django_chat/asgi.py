39bd220483f4d4934da0a98501410403ee8e21c2
JoynerRaj <joynerjohnraj04@gmail.com>
2026-03-27 12:41:25 +0530

    Fix Django setup in ASGI

--- django_chat/django_chat/asgi.py
+++ django_chat/django_chat/asgi.py
@@ -1,6 +1,9 @@
 import os
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
+
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
